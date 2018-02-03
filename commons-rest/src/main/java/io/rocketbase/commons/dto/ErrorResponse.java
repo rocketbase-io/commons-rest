e4a734f647afe22e67a75bd25e4a6c9c8ffef4f9
Marten Prieß <marten@rocketbase.io>
2018-02-04 00:52:39 +0100

    cleanedup lombok and removed builder

--- commons-rest/src/main/java/io/rocketbase/commons/dto/ErrorResponse.java
+++ commons-rest/src/main/java/io/rocketbase/commons/dto/ErrorResponse.java
@@ -1,20 +1,17 @@
 package io.rocketbase.commons.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor(onConstructor = @_(@JsonCreator))
-@Builder
 public class ErrorResponse {
 
     private int status;
     private String message;
 
-    @Singular
     private Map<String, String> fields;
 
     public ErrorResponse(int status, String message) {
--- commons-rest/src/main/java/io/rocketbase/commons/dto/PageableResult.java
+++ commons-rest/src/main/java/io/rocketbase/commons/dto/PageableResult.java
@@ -1,20 +1,13 @@
 package io.rocketbase.commons.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 
 import java.io.Serializable;
 import java.util.Iterator;
 import java.util.List;
 
-@Builder
 @Data
-@NoArgsConstructor
-@AllArgsConstructor(onConstructor = @_(@JsonCreator))
 public class PageableResult<E> implements Iterable<E>, Serializable {
 
     private long totalElements;
@@ -28,23 +21,23 @@ public class PageableResult<E> implements Iterable<E>, Serializable {
     private List<E> content;
 
     public static <E> PageableResult<E> contentPage(List<E> content, Page page) {
-        return PageableResult.<E>builder()
-                .content(content)
-                .totalPages(page.getTotalPages())
-                .totalElements(page.getTotalElements())
-                .page(page.getNumber())
-                .pageSize(page.getSize())
-                .build();
+        PageableResult result = new PageableResult();
+        result.setContent(content);
+        result.setTotalPages(page.getTotalPages());
+        result.setTotalElements(page.getTotalElements());
+        result.setPage(page.getNumber());
+        result.setPageSize(page.getSize());
+        return result;
     }
 
     public static <E> PageableResult<E> page(Page<E> page) {
-        return PageableResult.<E>builder()
-                .content(page.getContent())
-                .totalPages(page.getTotalPages())
-                .totalElements(page.getTotalElements())
-                .page(page.getNumber())
-                .pageSize(page.getSize())
-                .build();
+        PageableResult result = new PageableResult();
+        result.setContent(page.getContent());
+        result.setTotalPages(page.getTotalPages());
+        result.setTotalElements(page.getTotalElements());
+        result.setPage(page.getNumber());
+        result.setPageSize(page.getSize());
+        return result;
     }
 
     @Override
