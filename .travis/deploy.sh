1bc4b483af89b413f64042eb47c689561fb0c7ae
Marten Prieß <marten@rocketbase.io>
2018-02-05 12:02:48 +0100

    some build fixes

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,7 +9,7 @@ then
     cd commons-rest
 
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings ../.travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
+    mvn --settings ../.travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG -Prelease
 
     if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
         shred -v ~/.gnupg/*
