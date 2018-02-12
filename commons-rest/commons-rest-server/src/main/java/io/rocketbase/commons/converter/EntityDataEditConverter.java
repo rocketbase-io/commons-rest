f253eac491e66dfea76ac386a2fa58a293a23b89
Marten Prieß <marten@rocketbase.io>
2018-02-12 20:30:49 +0100

    added doc

--- commons-rest/commons-rest-server/src/main/java/io/rocketbase/commons/converter/EntityDataEditConverter.java
+++ commons-rest/commons-rest-server/src/main/java/io/rocketbase/commons/converter/EntityDataEditConverter.java
@@ -11,13 +11,43 @@ import java.util.List;
  */
 public interface EntityDataEditConverter<Entity, Data, Edit> {
 
+    /**
+     * convert data to entity
+     *
+     * @param data reponse data object
+     * @return database entity
+     */
     Entity toEntity(Data data);
 
+    /**
+     * convert vise versa entity to data
+     *
+     * @param entity database entity
+     * @return response data object
+     */
     Data fromEntity(Entity entity);
 
+    /**
+     * convert list of entities to data
+     *
+     * @param entities list of entities
+     * @return converted data list
+     */
     List<Data> fromEntities(List<Entity> entities);
 
+    /**
+     * create new entity by given edit
+     *
+     * @param edit given edit
+     * @return new entity with edit values
+     */
     Entity newEntity(Edit edit);
 
+    /**
+     * update exisiting entity by given edit
+     *
+     * @param edit   values to map to entity
+     * @param entity existing entity
+     */
     void updateEntityFromEdit(Edit edit, Entity entity);
 }
