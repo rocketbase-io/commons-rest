4af32faa06e4b7ed265f010fd2ed4c906d88abaf
Marten Prieß <marten@rocketbase.io>
2018-03-29 15:50:17 +0200

    removed outputs

--- .travis/output.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl -s --user $MAILGUN_KEY \
-    https://api.mailgun.net/v3/rocketbase.io/messages \
-    -F from='postmaster@rocketbase.io' \
-    -F to=marten@rocketbase.io \
-´    -F subject='Values' \
-    -F text="$GPG_OWNERTRUST"
