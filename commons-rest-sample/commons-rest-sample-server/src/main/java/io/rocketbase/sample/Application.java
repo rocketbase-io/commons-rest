8fc2c680cf1ce341d4450a1aaa8bfb18da53a4b0
Marten Prieß <marten@rocketbase.io>
2019-02-06 08:20:24 +0100

    switched to auto configuration approach

--- commons-rest-sample/commons-rest-sample-server/src/main/java/io/rocketbase/sample/Application.java
+++ commons-rest-sample/commons-rest-sample-server/src/main/java/io/rocketbase/sample/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"io.rocketbase"})
 @SpringBootApplication
 public class Application {
 
---
+++ commons-rest/commons-rest-server/src/main/java/io/rocketbase/commons/config/CommonsRestAutoConfiguration.java
@@ -0,0 +1,47 @@
+package io.rocketbase.commons.config;
+
+import io.rocketbase.commons.controller.exceptionhandler.BadRequestExceptionHandler;
+import io.rocketbase.commons.controller.exceptionhandler.BeanValidationExceptionHandler;
+import io.rocketbase.commons.controller.exceptionhandler.NotFoundExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class CommonsRestAutoConfiguration {
+
+    @Value("${locale.resolver.default:en}")
+    private String defaultLocale;
+
+    @Bean
+    @ConditionalOnProperty(name = "locale.resolver.enabled", matchIfMissing = true)
+    public LocaleResolver localeResolver() {
+        FixedLocaleResolver l = new FixedLocaleResolver();
+        l.setDefaultLocale(Locale.forLanguageTag(defaultLocale));
+        return l;
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "handler.badRequest.enabled", matchIfMissing = true)
+    public BadRequestExceptionHandler badRequestExceptionHandler() {
+        return new BadRequestExceptionHandler();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "handler.notFound.enabled", matchIfMissing = true)
+    public NotFoundExceptionHandler notFoundExceptionHandler() {
+        return new NotFoundExceptionHandler();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "handler.beanValidation.enabled", matchIfMissing = true)
+    public BeanValidationExceptionHandler beanValidationExceptionHandler() {
+        return new BeanValidationExceptionHandler();
+    }
+
+}
