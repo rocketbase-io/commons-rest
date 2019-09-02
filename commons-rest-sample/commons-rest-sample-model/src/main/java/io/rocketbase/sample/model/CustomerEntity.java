723e02aee4e3f78a36b0b924bce310021dfab259
Marten <marten@rocketbase.io>
2019-09-02 22:03:48 +0200

    changed to h2

--- commons-rest-sample/commons-rest-sample-model/src/main/java/io/rocketbase/sample/model/CustomerEntity.java
+++ commons-rest-sample/commons-rest-sample-model/src/main/java/io/rocketbase/sample/model/CustomerEntity.java
@@ -17,7 +17,7 @@ import javax.validation.constraints.Size;
 public class CustomerEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotNull
