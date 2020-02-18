109492f6acc613fdb712ec7327f43e0ce1d4f973
Marten <marten@rocketbase.io>
2020-02-18 21:59:15 +0100

    feat: added equals hashcode

--- commons-rest/commons-rest-api/src/main/java/io/rocketbase/commons/obfuscated/SimpleObfuscatedId.java
+++ commons-rest/commons-rest-api/src/main/java/io/rocketbase/commons/obfuscated/SimpleObfuscatedId.java
@@ -1,6 +1,7 @@
 package io.rocketbase.commons.obfuscated;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@EqualsAndHashCode(of = {"id", "obfuscated"})
 public class SimpleObfuscatedId implements ObfuscatedId {
 
     private Long id;
