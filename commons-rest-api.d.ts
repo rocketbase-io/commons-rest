5c681d835f656507c87038b690bc9b25d7fb7396
Marten <marten@rocketbase.io>
2020-03-04 10:20:50 +0100

    feat: added typescript d.ts file

---
+++ commons-rest-api.d.ts
@@ -0,0 +1,34 @@
+export interface ErrorResponse {
+    status: number;
+    message: string;
+    fields: Record<string, string[]>;
+}
+
+export interface PageableResult<E> {
+    totalElements: number;
+    totalPages: number;
+    page: number;
+    pageSize: number;
+    content: E[];
+}
+
+export interface AddressDto {
+    addressLineOne: string;
+    addressLineTwo: string;
+    city: string;
+    state: string;
+    postalCode: string;
+    countryCode: string;
+}
+
+export interface ContactDto {
+    gender: Gender;
+    title: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    landline: string;
+    cellphone: string;
+}
+
+export type Gender = "female" | "male" | "diverse";
