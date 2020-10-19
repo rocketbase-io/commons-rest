2926e011da0ae0f416ef61ab1e0006a2f702025f
Marten <marten@rocketbase.io>
2020-10-19 10:52:43 +0200

    feat: added keyValue + firstName lastName interface

---
+++ commons-rest/commons-rest-api/src/main/java/io/rocketbase/commons/model/HasFirstAndLastName.java
@@ -0,0 +1,34 @@
+package io.rocketbase.commons.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+
+
+public interface HasFirstAndLastName {
+
+    @Nullable
+    String getFirstName();
+
+    @Nullable
+    String getLastName();
+
+    /**
+     * combines first + last name<br>
+     * will return null in case firstName & lastName is empty
+     */
+    @JsonIgnore
+    default String getFullName() {
+        boolean emptyFirstName = StringUtils.isEmpty(getFirstName());
+        boolean emptyLastName = StringUtils.isEmpty(getLastName());
+        if (emptyFirstName && emptyLastName) {
+            return null;
+        } else if (!emptyFirstName && !emptyLastName) {
+            return String.format("%s %s", getFirstName(), getLastName());
+        } else if (!emptyFirstName) {
+            return getFirstName();
+        } else {
+            return getLastName();
+        }
+    }
+}
