554acf90335767a08c0df95733e35257e880b3b7
Marten <marten@rocketbase.io>
2020-11-26 13:50:56 +0100

    feat: updated sample project - compile java 11 + mapstruct 1.4.1

--- commons-rest-sample/commons-rest-sample-model/src/main/java/io/rocketbase/sample/converter/CentralConfig.java
+++ commons-rest-sample/commons-rest-sample-model/src/main/java/io/rocketbase/sample/converter/CentralConfig.java
@@ -1,12 +1,14 @@
 package io.rocketbase.sample.converter;
 
+import org.mapstruct.Builder;
 import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
 
 @MapperConfig(
         unmappedTargetPolicy = ReportingPolicy.ERROR,
         componentModel = "spring",
-        uses = {ObfuscatedIdMapper.class}
+        uses = {ObfuscatedIdMapper.class},
+        builder = @Builder(disableBuilder = true)
 )
 public interface CentralConfig {
 }
