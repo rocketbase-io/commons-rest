7a7376f88311b94bf7ca40243cef37f1289b19b4
Marten <marten@rocketbase.io>
2021-02-17 13:54:21 +0100

    ci: removed echo of variable

--- .travis/coverage.sh
+++ .travis/coverage.sh
@@ -5,8 +5,6 @@ sed -e 's/<[\/]\?group[^>]*>//g' coverage/target/site/jacoco-aggregate/jacoco.xm
 export JACOCO_SOURCE_PATH=$(find . -type d -regex '.*main/java' | xargs echo)
 export JACOCO_SOURCE_PATH="$JACOCO_SOURCE_PATH $(find . -type d -regex '.*target/generated-sources/annotations' | xargs echo)"
 
-echo $JACOCO_SOURCE_PATH
-
 ./cc-test-reporter format-coverage -t jacoco ./coverage/target/site/jacoco-aggregate/jacoco-cc.xml
 
 ./cc-test-reporter upload-coverage
