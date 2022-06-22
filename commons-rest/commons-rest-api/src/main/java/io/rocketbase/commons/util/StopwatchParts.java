1cb9f2b1ef827ae97eba9b649a715129a52620b6
Marten <marten@rocketbase.io>
2022-06-22 15:00:02 +0200

    feat: added StopwatchParts

---
+++ commons-rest/commons-rest-api/src/main/java/io/rocketbase/commons/util/StopwatchParts.java
@@ -0,0 +1,171 @@
+package io.rocketbase.commons.util;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static io.rocketbase.commons.util.StopwatchParts.PartOrdering.NATURAL;
+import static io.rocketbase.commons.util.StopwatchParts.PartOrdering.TIME_DESC;
+
+/**
+ * improved stopwatch for time-tracking that supports parts to know which area of a run tooks how long<br>
+ * sample output could look like:<br>
+ * ⏱ 4109 ms [29% syncValues: 1197 ms | 23% queryProduct: 953 ms | 21% colorMapping: 880 ms | 10% brainMap: 444 ms ...]
+ */
+public class StopwatchParts {
+
+    private StopwatchPartitionedConfig config;
+    private Map<String, Long> detailTimings;
+    private long lastLogged;
+
+    private String lastCaption;
+
+    StopwatchParts(StopwatchPartitionedConfig config) {
+        this.config = config;
+        detailTimings = new LinkedHashMap<>();
+        lastLogged = System.currentTimeMillis();
+    }
+
+    public static StopwatchParts start() {
+        return new StopwatchPartitionedConfig().start();
+    }
+
+    public static StopwatchPartitionedConfig build() {
+        return new StopwatchPartitionedConfig();
+    }
+
+    enum PartOrdering {
+        NATURAL, TIME_DESC;
+    }
+
+    @Getter(AccessLevel.PACKAGE)
+    static class StopwatchPartitionedConfig {
+
+        private String printPrefix = "⏱ ";
+        private PartOrdering partOrdering = TIME_DESC;
+        private boolean partPercentage = true;
+        private boolean partTimings = true;
+        private int limitParts = -1;
+
+        private boolean skippedSummary = true;
+        private String skippedPartName = "\"other\"";
+
+        StopwatchPartitionedConfig withPrefix(String printPrefix) {
+            this.printPrefix = printPrefix;
+            return this;
+        }
+
+        StopwatchPartitionedConfig withOrdered(PartOrdering partOrdering) {
+            this.partOrdering = partOrdering;
+            return this;
+        }
+
+        StopwatchPartitionedConfig withPercentage(boolean partPercentage) {
+            this.partPercentage = partPercentage;
+            return this;
+        }
+
+        StopwatchPartitionedConfig withTimings(boolean partTimings) {
+            this.partTimings = partTimings;
+            return this;
+        }
+
+        StopwatchPartitionedConfig limitParts(int limitParts) {
+            this.limitParts = limitParts;
+            return this;
+        }
+
+        StopwatchPartitionedConfig skippedPartName(String skippedPartName) {
+            this.skippedPartName = skippedPartName;
+            return this;
+        }
+
+        StopwatchPartitionedConfig withSkippedSummary(boolean skippedSummary) {
+            this.skippedSummary = skippedSummary;
+            return this;
+        }
+
+        StopwatchParts start() {
+            return new StopwatchParts(this);
+        }
+
+    }
+
+    /**
+     * if part is already existing it will get counted as total of part
+     */
+    public void part(String caption) {
+        long time = System.currentTimeMillis() - lastLogged;
+        detailTimings.put(caption, detailTimings.getOrDefault(caption, 0L) + time);
+        lastLogged = System.currentTimeMillis();
+        lastCaption = caption;
+    }
+
+    /**
+     * used to print all statistics as configured
+     */
+    public String print() {
+        if (lastCaption != null) {
+            // log last part as finished
+            part(lastCaption);
+        }
+        StringBuilder builder = new StringBuilder();
+        long totalTime = totalMillis();
+        builder.append(config.getPrintPrefix()).append(TimeUtil.convertMillisToMinSecFormat(totalTime));
+
+        List<Map.Entry<String, Long>> values = NATURAL.equals(config.getPartOrdering()) ? new ArrayList<>(detailTimings.entrySet()) : detailTimings.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).collect(Collectors.toList());
+        int partsToPrint = (config.getLimitParts() >= 0 ? Math.min(config.getLimitParts(), values.size()) : values.size()) - 1;
+
+        if (partsToPrint > 0) {
+            builder.append(" [");
+            for (int x = 0; x <= partsToPrint; x++) {
+                Map.Entry<String, Long> entry = values.get(x);
+                appendPart(builder, entry.getKey(), entry.getValue(), totalTime);
+                if (x != partsToPrint) {
+                    builder.append(" | ");
+                }
+            }
+
+            if (config.isSkippedSummary() && (values.size() - 1) > partsToPrint) {
+                long otherTotal = 0;
+                for (int x = partsToPrint + 1; x < values.size(); x++) {
+                    otherTotal += values.get(x).getValue();
+                }
+                builder.append(" | ");
+                appendPart(builder, config.getSkippedPartName(), otherTotal, totalTime);
+            }
+
+            builder.append("]");
+        }
+        return builder.toString();
+    }
+
+    private void appendPart(StringBuilder builder, String part, long time, long totalTime) {
+        if (config.isPartPercentage()) {
+            builder.append((int) ((time / (double) totalTime) * 100.0)).append("% ");
+        }
+        builder.append(part);
+        if (config.isPartTimings()) {
+            builder.append(": ").append(TimeUtil.convertMillisToMinSecFormat(time));
+        }
+    }
+
+    /**
+     * internal timing details - unmodifiable map
+     */
+    public Map<String, Long> details() {
+        return Collections.unmodifiableMap(detailTimings);
+    }
+
+    /**
+     * current total millis
+     */
+    public Long totalMillis() {
+        // used to get last part that has not been finished
+        long additional = System.currentTimeMillis() - lastLogged;
+        return detailTimings.values().stream().mapToLong(Long::longValue).sum() + additional;
+    }
+
+}
---
+++ commons-rest/commons-rest-api/src/test/java/io/rocketbase/commons/util/StopwatchPartsTest.java
@@ -0,0 +1,147 @@
+package io.rocketbase.commons.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.rocketbase.commons.util.StopwatchParts.PartOrdering.NATURAL;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.matchesPattern;
+
+class StopwatchPartsTest {
+
+    @Test
+    void printDefault() throws InterruptedException {
+        StopwatchParts stopwatch = StopwatchParts.start();
+        for (int x = 0; x < 10; x++) {
+            TimeUnit.MILLISECONDS.sleep(6 * x + 10);
+            stopwatch.part("part" + x);
+        }
+        String print = stopwatch.print();
+        assertThat(print, matchesPattern(
+                "⏱ [0-9]{2,3} ms \\[[0-9]{1,2}% part9\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part8\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part7\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part6\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part5\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part4\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part3\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part2\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part1\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part0\\: [0-9]{2,3} ms\\]"));
+
+    }
+
+    @Test
+    void printConfiguredNaturalOrder() throws InterruptedException {
+        StopwatchParts stopwatch = StopwatchParts.build()
+                .withOrdered(NATURAL)
+                .start();
+        for (int x = 0; x < 10; x++) {
+            TimeUnit.MILLISECONDS.sleep(6 * x + 10);
+            stopwatch.part("part" + x);
+        }
+        String print = stopwatch.print();
+        assertThat(print, matchesPattern(
+                "⏱ [0-9]{2,3} ms \\[[0-9]{1,2}% part0\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part1\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part2\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part3\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part4\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part5\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part6\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part7\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part8\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part9\\: [0-9]{2,3} ms\\]"));
+    }
+
+    @Test
+    void printConfiguredLimit() throws InterruptedException {
+        StopwatchParts stopwatch = StopwatchParts.build()
+                .limitParts(4)
+                .withSkippedSummary(false)
+                .start();
+        for (int x = 0; x < 10; x++) {
+            TimeUnit.MILLISECONDS.sleep(6 * x + 10);
+            stopwatch.part("part" + x);
+        }
+        String print = stopwatch.print();
+        assertThat(print, matchesPattern(
+                "⏱ [0-9]{2,3} ms \\[[0-9]{1,2}% part9\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part8\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part7\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part6\\: [0-9]{2,3} ms\\]"));
+    }
+
+    @Test
+    void printConfiguredLimitCustomOrder() throws InterruptedException {
+        StopwatchParts stopwatch = StopwatchParts.build()
+                .limitParts(3)
+                .withOrdered(NATURAL)
+                .withSkippedSummary(false)
+                .start();
+        for (int x = 0; x < 10; x++) {
+            TimeUnit.MILLISECONDS.sleep(6 * x + 10);
+            stopwatch.part("part" + x);
+        }
+        String print = stopwatch.print();
+        assertThat(print, matchesPattern(
+                "⏱ [0-9]{2,3} ms \\[[0-9]{1,2}% part0\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part1\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part2\\: [0-9]{2,3} ms\\]"));
+    }
+
+    @Test
+    void printConfiguredNoPercentage() throws InterruptedException {
+        StopwatchParts stopwatch = StopwatchParts.build()
+                .limitParts(3)
+                .withOrdered(NATURAL)
+                .withPercentage(false)
+                .withSkippedSummary(false)
+                .start();
+        for (int x = 0; x < 10; x++) {
+            TimeUnit.MILLISECONDS.sleep(6 * x + 10);
+            stopwatch.part("part" + x);
+        }
+        String print = stopwatch.print();
+        assertThat(print, matchesPattern(
+                "⏱ [0-9]{2,3} ms \\[part0\\: [0-9]{2,3} ms \\| part1\\: [0-9]{2,3} ms \\| part2\\: [0-9]{2,3} ms\\]"));
+    }
+
+    @Test
+    void printConfiguredNoTimings() throws InterruptedException {
+        StopwatchParts stopwatch = StopwatchParts.build()
+                .limitParts(3)
+                .withOrdered(NATURAL)
+                .withTimings(false)
+                .withSkippedSummary(false)
+                .start();
+        for (int x = 0; x < 10; x++) {
+            TimeUnit.MILLISECONDS.sleep(6 * x + 10);
+            stopwatch.part("part" + x);
+        }
+        String print = stopwatch.print();
+        assertThat(print, matchesPattern(
+                "⏱ [0-9]{2,3} ms \\[[0-9]{1,2}% part0 \\| [0-9]{1,2}% part1 \\| [0-9]{1,2}% part2\\]"));
+    }
+
+    @Test
+    void printConfiguredNoParts() throws InterruptedException {
+        StopwatchParts stopwatch = StopwatchParts.build()
+                .limitParts(0)
+                .start();
+        for (int x = 0; x < 10; x++) {
+            TimeUnit.MILLISECONDS.sleep(6 * x + 10);
+            stopwatch.part("part" + x);
+        }
+        String print = stopwatch.print();
+        assertThat(print, matchesPattern(
+                "⏱ [0-9]{2,3} ms"));
+    }
+
+    @Test
+    void printConfiguredOtherPrefix() throws InterruptedException {
+        StopwatchParts stopwatch = StopwatchParts.build()
+                .limitParts(0)
+                .withPrefix("🎉 ")
+                .start();
+        for (int x = 0; x < 10; x++) {
+            TimeUnit.MILLISECONDS.sleep(6 * x + 10);
+            stopwatch.part("part" + x);
+        }
+        String print = stopwatch.print();
+        assertThat(print, matchesPattern(
+                "🎉 [0-9]{2,3} ms"));
+    }
+
+    @Test
+    void printConfiguredLimitSummary() throws InterruptedException {
+        StopwatchParts stopwatch = StopwatchParts.build()
+                .limitParts(3)
+                .start();
+        for (int x = 0; x < 10; x++) {
+            TimeUnit.MILLISECONDS.sleep(6 * x + 10);
+            stopwatch.part("part" + x);
+        }
+        String print = stopwatch.print();
+        assertThat(print, matchesPattern(
+                "⏱ [0-9]{2,3} ms \\[[0-9]{1,2}% part9\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part8\\: [0-9]{2,3} ms \\| [0-9]{1,2}% part7\\: [0-9]{2,3} ms \\| [0-9]{1,2}% \"other\"\\: [0-9]{2,3} ms\\]"));
+    }
+}
